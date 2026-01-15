55f5c880923613a3a384e994bfddbbaa585203f3
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2026-01-15 14:44:25 +0100

    Refactor mzhash192 function and update metadata
    Updated the mzhash192 function to improve performance and added a string version. Changed author date and license information.

--- src/C/mzhash192.c
+++ src/C/mzhash192.c
@@ -1,54 +1,55 @@
 /**
- * mzHash192
+ * mzhash192
  * 
  * Strong, fast, simple, non-cryptographic 192-bit hash function
  * 
  * Author: Matteo Zapparoli
- * Date: 2025
- * Licence: Public Domain
- * 
- * This is free and unencumbered software released into the public domain.
+ * Date: 2026-01
+ * Reelease: 2
  *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- * 
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * 
- * For more information, please refer to <https://unlicense.org>
+ * SPDX-FileCopyrightText: 2026 Matteo Zapparoli <zapparoli.matteo@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE file in project root.
  * 
  */
 
 #include "mzhash192.h"
 
-void mzhash192(const char* data, size_t length, uint64_t* output)
+void mzhash192(const void* data, size_t length, uint64_t* output)
 {
-	uint64_t hash1;
-	uint64_t hash2 = 0x7F573AFD9B2368FDL;
-	uint64_t hash3 = 0xEBCDBA32A30D97ACL;
-		
-	for(size_t i = 0; i < length; i++) {
-		uint64_t x = i + data[i];
-		hash1 = 0x91C1BACD9E6787F3L * x ^ (hash3 << 2) ^ (hash3 >> 2);
-		hash2 = 0x447239684A147E94L * x ^ (hash2 << 2) ^ (hash2 >> 2);
-		hash3 = 0xFAADD3787DABBC1EL * x ^ (hash1 << 2) ^ (hash1 >> 2);
+	const int8_t *bytes = (const int8_t*)data;
+	uint64_t hash0 = 0xB04C2438F4F7D8D1uLL;
+	uint64_t hash1 = 0xA3B8FD0DF0836C0DuLL;
+	uint64_t hash2 = 0x9E9BDFDAEFC3A606uLL;
+        
+	while(length--) {
+		uint64_t h0 = 0xD76F648260B0F9FDuLL * (*bytes ^ (hash0 << 8) ^ (hash0 >> 8));
+		uint64_t h1 = 0xD1DA2131A0C25299uLL * (*bytes ^ (hash1 << 8) ^ (hash1 >> 8));
+		hash0 = 0x188EF276E8755C0FuLL * (*bytes++ ^ (hash2 << 8) ^ (hash2 >> 8));
+		hash1 = h0;
+		hash2 = h1;
 	}
-	output[0] = hash1;
-	output[1] = hash2;
-	output[2] = hash3;
+	
+	output[0] = hash0;
+	output[1] = hash1;
+	output[2] = hash2;
+}
+
+void mzhash192_str(const char* str, uint64_t* output)
+{
+	uint64_t hash0 = 0xB04C2438F4F7D8D1uLL;
+	uint64_t hash1 = 0xA3B8FD0DF0836C0DuLL;
+	uint64_t hash2 = 0x9E9BDFDAEFC3A606uLL;
+		
+	while(*str) {
+		uint64_t h0 = 0xD76F648260B0F9FDuLL * (*str ^ (hash0 << 8) ^ (hash0 >> 8));
+		uint64_t h1 = 0xD1DA2131A0C25299uLL * (*str ^ (hash1 << 8) ^ (hash1 >> 8));
+		hash0 = 0x188EF276E8755C0FuLL * (*str++ ^ (hash2 << 8) ^ (hash2 >> 8));
+		hash1 = h0;
+		hash2 = h1;
+	}
+	
+	output[0] = hash0;
+	output[1] = hash1;
+	output[2] = hash2;
 }
