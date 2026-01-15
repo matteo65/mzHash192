3bae8c42df8c116f2343b8c0229e484868b08532
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2026-01-15 14:04:35 +0100

    Update HashFunct192 with new hash algorithm

--- src/java/HashFunct192.java
+++ src/java/HashFunct192.java
@@ -1,76 +1,62 @@
 /**
- * mzHash192
+ * mzhash192
  * 
  * Strong, fast, simple, non-cryptographic 192-bit hash function
  * 
  * Author: Matteo Zapparoli
- * Date: 2025
- * Licence: Public Domain
- * 
- * This is free and unencumbered software released into the public domain.
+ * Date: 2026-01
+ * Reelease: 2
  *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- * 
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * 
- * For more information, please refer to <https://unlicense.org>
+ * SPDX-FileCopyrightText: 2026 Matteo Zapparoli <zapparoli.matteo@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE file in project root.
  * 
  */
+
 public class HashFunct192 {
 	
 	public static class ThreeLongs {
+		public long val0;
 		public long val1;
 		public long val2;
-		public long val3;
 	}
 	
+	private HashFunct192() {}
+	
 	public static void mzHash192(byte[] data, int start, int length, ThreeLongs out) {
-		long hash1 = 0;
-		long hash2 = 0x7F573AFD9B2368FDL;
-		long hash3 = 0xEBCDBA32A30D97ACL;
+		long hash0 = 0xB04C2438F4F7D8D1L;
+		long hash1 = 0xA3B8FD0DF0836C0DL;
+		long hash2 = 0x9E9BDFDAEFC3A606L;
 		
 		for(int i = 0; i < length; i++) {
-			long x = i + data[start + i];
-			hash1 = 0x91C1BACD9E6787F3L * x ^ (hash3 << 2) ^ (hash3 >>> 2);
-			hash2 = 0x447239684A147E94L * x ^ (hash2 << 2) ^ (hash2 >>> 2);
-			hash3 = 0xFAADD3787DABBC1EL * x ^ (hash1 << 2) ^ (hash1 >>> 2);
+            byte b = data[start + i];
+            long h0 = 0xD76F648260B0F9FDL * (b ^ (hash0 << 8) ^ (hash0 >>> 8));
+            long h1 = 0xD1DA2131A0C25299L * (b ^ (hash1 << 8) ^ (hash1 >>> 8));
+            hash0 = 0x188EF276E8755C0FL * (b ^ (hash2 << 8) ^ (hash2 >>> 8));
+            hash1 = h0;
+            hash2 = h1;
 		}
+		out.val0 = hash0;
 		out.val1 = hash1;
 		out.val2 = hash2;
-		out.val3 = hash3;
 	}
 	
 	public static void mzHash192(byte[] data, int start, int length, long[] out) {
-		long hash1 = 0;
-		long hash2 = 0x7F573AFD9B2368FDL;
-		long hash3 = 0xEBCDBA32A30D97ACL;
-		
-		for(int i = 0; i < length; i++) {
-			long x = i + data[start + i];
-			hash1 = 0x91C1BACD9E6787F3L * x ^ (hash3 << 2) ^ (hash3 >>> 2);
-			hash2 = 0x447239684A147E94L * x ^ (hash2 << 2) ^ (hash2 >>> 2);
-			hash3 = 0xFAADD3787DABBC1EL * x ^ (hash1 << 2) ^ (hash1 >>> 2);
-		}
-		out[0] = hash1;
-		out[1] = hash2;
-		out[2] = hash3;
+        long hash0 = 0xB04C2438F4F7D8D1L;
+        long hash1 = 0xA3B8FD0DF0836C0DL;
+        long hash2 = 0x9E9BDFDAEFC3A606L;
+        
+        for(int i = 0; i < length; i++) {
+            byte b = data[start + i];
+            long h0 = 0xD76F648260B0F9FDL * (b ^ (hash0 << 8) ^ (hash0 >>> 8));
+            long h1 = 0xD1DA2131A0C25299L * (b ^ (hash1 << 8) ^ (hash1 >>> 8));
+            hash0 = 0x188EF276E8755C0FL * (b ^ (hash2 << 8) ^ (hash2 >>> 8));
+            hash1 = h0;
+            hash2 = h1;
+        }
+        out[0] = hash0;
+        out[1] = hash1;
+        out[2] = hash2;
 	}
 
 	public static long[] mzHash192(byte[] data, int start, int length) {
